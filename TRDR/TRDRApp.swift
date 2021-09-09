238a77ece261431d615f29e60ebc42c7e2752ebd
Robbie Paine <robbie.paine@yahoo.com>
2021-09-09 09:14:03 -0400

    Initial Commit

---
+++ TRDR/TRDRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRDRApp.swift
+//  TRDR
+//
+//  Created by Robbie on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TRDRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
